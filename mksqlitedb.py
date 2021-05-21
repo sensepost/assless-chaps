f645f3517984fc966285ce347aa9f9eadb787d48
singe <singe-github@singe.za.net>
2021-05-21 10:10:11 +0200

    Changed mksqlitedb to create the index after the import to speed things up.

--- mksqlitedb.py
+++ mksqlitedb.py
@@ -3,6 +3,10 @@ import sqlite3
 from sys import argv
 import subprocess
 
+# NB create the index after import, or you'll have a slow time
+
+print(f'[-] Creating database {argv[1]} from CSV file {argv[2]}')
+
 with sqlite3.connect(argv[1]) as db:
   cur = db.cursor()
   cur.execute('CREATE TABLE IF NOT EXISTS hashes \
@@ -10,8 +14,18 @@ with sqlite3.connect(argv[1]) as db:
     chunk1 TEXT NOT NULL COLLATE NOCASE,\
     chunk2 TEXT NOT NULL COLLATE NOCASE);\
   ')
-  cur.execute('CREATE INDEX IF NOT EXISTS tb ON hashes (twobytes);')
+  #cur.execute('CREATE INDEX IF NOT EXISTS tb ON hashes (twobytes);')
+
+print('[-] Starting import, this can take a long time if the CSV is large')
 
 subprocess.call(["sqlite3", argv[1], 
   ".mode csv", 
   f'.import {argv[2]} hashes'])
+
+print('[-] Creating index ...')
+
+with sqlite3.connect(argv[1]) as db:
+  cur = db.cursor()
+  cur.execute('CREATE INDEX IF NOT EXISTS tb ON hashes (twobytes);')
+
+print('[+] All done')
